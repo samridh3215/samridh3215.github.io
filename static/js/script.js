fb5c253c54de2a52eca183cbd550150f0d2420a1
Samridh Anand <samridhanand.2003@gmail.com>
2023-12-24 22:11:45 +0530

    added a project

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,22 @@ fetch('/static/content.json').then(res=>res.json()).then(json=>{
         node.className = "skill-image highlight"
         document.getElementById('skill-container').appendChild(node)
     });
+
+    json['projects'].forEach((element)=>{
+        let node = document.createElement("div")
+        node.className = 'project-card'
+        let title = document.createElement("h3")
+        title.textContent = element['title']
+        let desc = document.createElement('p')
+        desc.textContent = element['desc']
+        let anchor = document.createElement("a")
+        anchor.href = element['link']
+        anchor.target = "_blank"
+        let button = document.createElement("button")
+        button.textContent = "Github"
+        button.className='github'
+        anchor.appendChild(button)
+        node.append(title, desc, anchor)
+        document.getElementById('projects-holder').appendChild(node)
+    })
 })
